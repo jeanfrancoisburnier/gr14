2ed5e4ae58d6ff9786fc4af634dc4ebe470a7122
jeanfrancoisburnier <jean-francois.burnier@epfl.ch>
2016-10-05 13:15:18 +0200

    add PI controller for speed reuglation

--- regulation/speed_regulation_gr14.cc
+++ regulation/speed_regulation_gr14.cc
@@ -33,10 +33,18 @@ void speed_regulation(CtrlStruct *cvs, double r_sp_ref, double l_sp_ref)
 
 	// ----- Wheels regulation computation start ----- //
 
-	// wheel commands
-	outputs->wheel_commands[R_ID] = 0.0;
-	outputs->wheel_commands[L_ID] = 0.0;
+	double TI = 0.5;
+	double KP = 50.0;
 
+	sp_reg->int_error_r = sp_reg->int_error_r + r_sp_ref-r_sp;
+	sp_reg->int_error_l = sp_reg->int_error_l + l_sp_ref-l_sp;
+
+	// wheel commands
+	outputs->wheel_commands[R_ID] = KP*(r_sp_ref-r_sp + sp_reg->int_error_r*dt/TI);
+	outputs->wheel_commands[L_ID] = KP*(l_sp_ref-l_sp + sp_reg->int_error_l*dt/TI);
+
+	outputs->wheel_commands[R_ID] = limit_range(outputs->wheel_commands[R_ID], -100, 100);
+	outputs->wheel_commands[L_ID] = limit_range(outputs->wheel_commands[L_ID], -100, 100);
 	// ----- Wheels regulation computation end ----- //
 
 	// last update time
