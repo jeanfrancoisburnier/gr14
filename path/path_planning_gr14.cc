6aa24451e7d2a139931943b8cbe674626a89ec0a
AdrienThabuis <adrien.thabuis@epfl.ch>
2016-11-16 15:55:35 +0100

    debut du pathplanning, mise en place de la map, class etc ...

--- path/path_planning_gr14.cc
+++ path/path_planning_gr14.cc
@@ -4,6 +4,8 @@
 #include "useful_gr14.h"
 #include <math.h>
 
+#define NB_OBSTACLES 10 // 8 fixes obstacles + 2 oponents
+
 NAMESPACE_INIT(ctrlGr14);
 
 /*! \brief initialize the path-planning algorithm (memory allocated)
@@ -19,6 +21,62 @@ PathPlanning* init_path_planning()
 
 	// ----- path-planning initialization start ----- //
 
+	//creation of the obstacles
+	array<Obstacles, NB_OBSTACLES> list_obstacles;
+	list_obstacles[0].first_corner[X] = -0.850;
+	list_obstacles[0].first_corner[Y] = 1.0;
+	list_obstacles[0].second_corner[X] = -0.350;
+	list_obstacles[0].second_corner[Y] = 0.680;
+
+	list_obstacles[1].first_corner[X] = 0.330;
+	list_obstacles[1].first_corner[Y] = 1.350;
+	list_obstacles[1].second_corner[X] = 0.650;
+	list_obstacles[1].second_corner[Y] = 0.850;
+
+	list_obstacles[2].first_corner[X] = 0.330;
+	list_obstacles[2].first_corner[Y] = -0.850;
+	list_obstacles[2].second_corner[X] = 0.650;
+	list_obstacles[2].second_corner[Y] = -1.350;
+
+	list_obstacles[3].first_corner[X] = -0.850;
+	list_obstacles[3].first_corner[Y] = -0.680;
+	list_obstacles[3].second_corner[X] = -0.350;
+	list_obstacles[3].second_corner[Y] = -1.0;
+
+	list_obstacles[4].first_corner[X] = -0.350;
+	list_obstacles[4].first_corner[Y] = 0.550;
+	list_obstacles[4].second_corner[X] = 0.350;
+	list_obstacles[4].second_corner[Y] = 0.150;
+
+	list_obstacles[5].first_corner[X] = -0.350;
+	list_obstacles[5].first_corner[Y] = 0.150;
+	list_obstacles[5].second_corner[X] = 0.050;
+	list_obstacles[5].second_corner[Y] = -0.150;
+
+	list_obstacles[6].first_corner[X] = -0.650;
+	list_obstacles[6].first_corner[Y] = 0.250;
+	list_obstacles[6].second_corner[X] = -0.350;
+	list_obstacles[6].second_corner[Y] = -0.250;
+
+	list_obstacles[7].first_corner[X] = -0.350;
+	list_obstacles[7].first_corner[Y] = -0.150;
+	list_obstacles[7].second_corner[X] = 0.350;
+	list_obstacles[7].second_corner[Y] = -0.550;
+
+
+	//oponents (do not have any value for now --> would be great to initialize with the value computed with the lazer tower)
+	list_obstacles[8].first_corner[X] = 0.00;
+	list_obstacles[8].first_corner[Y] = 0.00;
+	list_obstacles[8].second_corner[X] = 0.00;
+	list_obstacles[8].second_corner[Y] = 0.00;
+
+	list_obstacles[9].first_corner[X] = 0.00;
+	list_obstacles[9].first_corner[Y] = 0.00;
+	list_obstacles[9].second_corner[X] = 0.00;
+	list_obstacles[9].second_corner[Y] = 0.00;
+
+
+	
 
 	// ----- path-planning initialization end ----- //
 
--- path/path_planning_gr14.h
+++ path/path_planning_gr14.h
@@ -4,11 +4,24 @@
  * \brief path-planning algorithm
  */
 
+
+
 #ifndef _PATH_PLANNING_GR14_H_
 #define _PATH_PLANNING_GR14_H_ 
  
 #include "namespace_ctrl.h"
 #include "CtrlStruct_gr14.h"
+
+#include <vector>
+#include <array>
+
+
+#define SQUARE_SIZE 50 //[mm] dimensions of the grid's squares
+#define X 0 	//index of the x position in the array coordinates
+#define Y 1 	//index of the y position in the array coordinates
+
+using namespace std; //to be able to use array
+
 
 NAMESPACE_INIT(ctrlGr14);
 
@@ -17,6 +30,100 @@ struct PathPlanning
 {
 	int dummy_variable; ///< put your own variable, this is just an example without purpose
 };
+
+
+
+
+
+
+//rectangle delimited by two extreme corner (first : top left, second : bottom right) 
+struct Obstacles 
+{
+	array<float, 2> first_corner;
+	array<float, 2> second_corner;
+};
+
+
+
+
+
+//Ede object contained in a Node Object
+class Edge
+{
+private:
+	int id;					//current edge's id
+	float weight;			//correspond to the length of the edge (twos differents values for us 1 or sqrt(2))
+
+public:
+	int edge_get_id()
+	{
+		return id;
+	}
+
+	float edge_get_weight()
+	{
+		return weight;
+	}
+
+
+	Edge::Edge(int id_edge, float weight_edge)
+	{
+		id = id_edge;
+		weight = weight_edge;
+
+	}
+
+	Edge::~Edge()
+	{}
+};
+
+
+
+
+
+
+//Node object containing among other things eight Edge objects
+class Node
+{
+private:
+	int id;							//current node's id
+	int previous_node_id;			//id of the previous node visited
+	bool visited = false;			//true if this node havs been already during this pathplanning and false otherwise 
+	bool free_position;				//indicate if the node is accessible or not
+	array<float, 2> coordinates;	//coordinates of the node
+	vector<Edge> direct_edges;	//differents edges of the Node (connection to other nodes)
+
+public:
+	int distance_to_start;	//total distance already travelled from the start (to see if we go in the good direction)
+
+
+	Node::Node(int id_node, int* ptr_total_edge_index, bool free_init)
+	{
+		id = id_node;
+		free_position = free_init;
+		
+		//coordinates[X] = calcul with the id_repartition and the number of squares
+		//coordinates[Y] = calcul with the id_repartition and the number of squares
+
+		//create the right amount of edeges : number edges --> calcul the particular cases
+		//int number edges = 8;
+		//set the number of edges so the size of vector direct_edges
+
+		//Do the initialization of the different edges
+
+	}
+
+	Node::~Node()
+	{}
+};
+
+
+
+
+
+
+
+
 
 PathPlanning* init_path_planning();
 void free_path_planning(PathPlanning *path);
